4b54d6e22746441dcb72664b880057d7630771c9
k0n0di0d4 <k00n00di00d4@gmail.com>
2023-08-30 20:36:49 +0200

    eeee

--- api_gateway/src/main/java/com/example/api_gateway/controller/ApiGatewayController.java
+++ api_gateway/src/main/java/com/example/api_gateway/controller/ApiGatewayController.java
@@ -1,4 +1,23 @@
 package com.example.api_gateway.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class ApiGatewayController {
+
+    @GetMapping("/register")
+    public ResponseEntity<String> register() {
+        return ResponseEntity.ok("hello");
+    }
+
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<AuthenticationResponse> authenticate(
+//            @RequestBody AuthenticationRequest request
+//    ) {
+//        return ResponseEntity.ok(authenticationService.authentication(request));
+//    }
 }

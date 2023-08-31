ccaaa9918af9ffc860ab493cc05fad369b59a982
k0n0di0d4 <k00n00di00d4@gmail.com>
2023-08-31 19:37:41 +0200

    3/4 eureka done

--- authentication_service/src/main/java/com/example/authentication_service/AuthenticationServiceApplication.java
+++ authentication_service/src/main/java/com/example/authentication_service/AuthenticationServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 

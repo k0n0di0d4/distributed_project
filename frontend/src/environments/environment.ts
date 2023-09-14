6905cf3c337e452b5710d4292d07d37cd59570f2
Daniel Skrobot <dd7412506@gmail.com>
2023-09-15 01:45:53 +0200

    fix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'http://localhost:19726'
 };
 // baseUrl: 'http://localhost:19726'
-
+//baseUrl: 'http://localhost:8081'
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
